2dff58a64fed963e9068a01b67c2ddfabc0015de
ijh1298 <imjh0417@gmail.com>
2024-12-11 16:22:09 +0900

    feat: InputView 구현

--- src/main/kotlin/bridge/InputView.kt
+++ src/main/kotlin/bridge/InputView.kt
@@ -1,27 +1,20 @@
 package bridge
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
+import camp.nextstep.edu.missionutils.Console
+
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
-    fun readBridgeSize(): Int {
-        return 0
+    fun readBridgeSize(): String {
+        println("다리의 길이를 입력해주세요.")
+        return Console.readLine()
     }
 
-    /**
-     * 사용자가 이동할 칸을 입력받는다.
-     */
     fun readMoving(): String {
-        return ""
+        println("이동할 칸을 선택해주세요. (위: U, 아래: D)")
+        return Console.readLine()
     }
 
-    /**
-     * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
-     */
     fun readGameCommand(): String {
-        return ""
+        println("게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)")
+        return Console.readLine()
     }
 }
--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -4,11 +4,10 @@ package bridge
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 class OutputView {
-    /**
-     * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
+    fun printStartMessage() {
+        println("다리 건너기 게임을 시작합니다.")
+    }
+
     fun printMap() {}
 
     /**
