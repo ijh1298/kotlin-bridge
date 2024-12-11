64496e6b8f87fa5e5ef8c3ee69466747e1c0f13d
ijh1298 <imjh0417@gmail.com>
2024-12-11 17:36:49 +0900

    feat: BridgeGameController 내 게임 실행 로직 구현

--- src/main/kotlin/bridge/controller/BridgeGameController.kt
+++ src/main/kotlin/bridge/controller/BridgeGameController.kt
@@ -1,18 +1,62 @@
 package bridge.controller
 
+import bridge.BridgeGame
+import bridge.BridgeMaker
+import bridge.utils.BridgeRandomNumberGenerator
 import bridge.validator.InputValidator
 import bridge.view.InputView
 import bridge.view.OutputView
 
 class BridgeGameController(
     private val inputView: InputView = InputView(),
-    private val outputView: OutputView = OutputView()
+    private val outputView: OutputView = OutputView(),
+    private val bridgeMaker: BridgeMaker = BridgeMaker(BridgeRandomNumberGenerator())
 ) {
+    private lateinit var game: BridgeGame
+    private lateinit var bridge: List<String>
     private var bridgeLength = 3
     private var gameAgain = false
+    private var currentTurn = 0
 
     fun run() {
-        inputBridgeLength()
+        do {
+            inputBridgeLength()
+            setNewBridge()
+            game = BridgeGame(bridge)
+            println(bridge.toString())
+            playGame()
+            inputGameAgain()
+        } while (gameAgain)
+    }
+
+    private fun playGame() {
+        while (!game.isDone) {
+            val result = game.move(inputMoving(), currentTurn++)
+            outputView.printMap(result)
+        }
+        game.retry()
+    }
+
+    private fun inputGameAgain() {
+        var again = ""
+        loopUntilValid {
+            again = inputView.readGameCommand()
+            InputValidator.validateCommand(again)
+        }
+        if (again == "R") {
+            gameAgain = true
+            return
+        }
+        gameAgain = false
+    }
+
+    private fun inputMoving(): String {
+        var moving = ""
+        loopUntilValid {
+            moving = inputView.readMoving()
+            InputValidator.validateMoving(moving)
+        }
+        return moving
     }
 
     private fun inputBridgeLength() {
@@ -22,6 +66,10 @@ class BridgeGameController(
             InputValidator.validateBridgeLength(rawLength)
         }
         bridgeLength = rawLength.toInt()
+    }
+
+    private fun setNewBridge() {
+        bridge = bridgeMaker.makeBridge(bridgeLength)
     }
 
     // 반복 입력받는 로직
