8d3ee2edd920976a2633b546ab49c565e27da3fa
ijh1298 <imjh0417@gmail.com>
2024-12-11 17:36:17 +0900

    feat: OutputView 내 printMap 메소드 구현

--- src/main/kotlin/bridge/view/OutputView.kt
+++ src/main/kotlin/bridge/view/OutputView.kt
@@ -8,12 +8,11 @@ class OutputView {
         println("다리 건너기 게임을 시작합니다.")
     }
 
-    fun printMap() {}
+    fun printMap(bridges: Pair<List<String>, List<String>>) {
+        val upBridge = bridges.first.joinToString(" | ")
+        val downBridge = bridges.second.joinToString(" | ")
+        println("[ $upBridge ]\n[ $downBridge ]")
+    }
 
-    /**
-     * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
-     *
-     * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
-     */
     fun printResult() {}
 }
