f5f0f89d9f2e48a4f031d7999a1ba57c42becacf
ijh1298 <imjh0417@gmail.com>
2024-12-11 17:18:25 +0900

    feat: BridgeGame 구현

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -1,7 +1,46 @@
 package bridge
 
-class BridgeGame {
-    fun move() {}
+class BridgeGame(
+    private val bridge: List<String>
+) {
+    var isDone = false
+    private val upBridge = mutableListOf<String>()
+    private val bottomBridge = mutableListOf<String>()
+
+    fun move(command: String, index: Int): Pair<List<String>, List<String>> {
+        val isCorrect = command == bridge[index]
+        updateUpBridge(command, isCorrect)
+        updateBottomBridge(command, isCorrect)
+
+        if (!isCorrect || index == bridge.size - 1)
+            isDone = true
+
+        return upBridge to bottomBridge
+    }
 
     fun retry() {}
+
+    private fun updateUpBridge(command: String, isCorrect: Boolean) {
+        if (command == "U" && isCorrect) {
+            upBridge += "O"
+            return
+        }
+        if (isCorrect) {
+            upBridge += ""
+            return
+        }
+        upBridge += "X"
+    }
+
+    private fun updateBottomBridge(command: String, isCorrect: Boolean) {
+        if (command == "D" && isCorrect) {
+            bottomBridge += "O"
+            return
+        }
+        if (isCorrect) {
+            bottomBridge += ""
+            return
+        }
+        bottomBridge += "X"
+    }
 }
